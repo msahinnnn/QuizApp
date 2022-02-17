b2fd9b1f999a775d4ac05acecdbdc408c97fd28c
msahinnnn <mehemt1907cr7@gmail.com>
2022-02-18 00:46:19 +0300

    Add project files.

---
+++ QuizApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Question> questionList = new List<Question>();
+
+            Question question1 = new Question("Sport" , "Who is Michael Jordan?" , "A" , "Basketball Player" , "Artist" , "Singer" , "Politican");
+            Question question2 = new Question("Math", "2 + 2 = ?", "D", "1", "2", "3", "4");
+            Question question3 = new Question("Math", "2 - 3 = ?", "A", "-1", "0", "1", "3");
+            Question question4 = new Question("Sport", "Who is Cristiano Ronaldo?", "B", "Movie Player", "Football Player", "Singer", "Politican");
+            Question question5 = new Question("Movie", "Iron-Man's name?", "B", "Walter White", "Tony Stark", "John Doe", "Bruce Wayne");
+
+            questionList.Add(question1);
+            questionList.Add(question2);
+            questionList.Add(question3);
+            questionList.Add(question4);
+            questionList.Add(question5);
+
+            int i = 1;
+            int point = 0;
+            int pointPerQuestion = 100 / questionList.Count;
+            foreach (var question in questionList)
+            {
+                
+                
+                Console.WriteLine("Question " + i  + ":");
+                Console.WriteLine("Question Type : " + question.QuestionType);
+                Console.WriteLine("Question : " + question.QuestionText);               
+                Console.WriteLine("Please answer the question?");
+                Console.WriteLine();
+                Console.WriteLine("(A) " + question.QuestionAnswerA);
+                Console.WriteLine("(B) " + question.QuestionAnswerB);
+                Console.WriteLine("(C) " + question.QuestionAnswerC);
+                Console.WriteLine("(D) " + question.QuestionAnswerD);
+                string answer = Console.ReadLine();
+                string correctAnswer = question.QuestionAnswer.ToString();          
+                bool correctCorrect = correctAnswer.Equals(answer);               
+                if(answer == "A"  || answer == "B" || answer == "C" || answer == "D")
+                {
+                    if (correctCorrect)
+                    {
+                        point += pointPerQuestion;
+                    }                   
+                }
+                else
+                {
+                    Console.Write("Please answer in the correct format !");
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine("**********************");
+                i += 1;
+                
+            }
+
+            Console.WriteLine("Total Point = " + point);
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
---
+++ QuizApp/Question.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    public class Question
+    {
+        private string questionType;
+        private string questionText;
+        private string questionAnswer;
+        private string questionAnswerA;
+        private string questionAnswerB;
+        private string questionAnswerC;
+        private string questionAnswerD;
+
+        public Question(string questiontype, string questiontext, string questionanswer, string questionanswera, string questionanswerb, string questionanswerc, string questionanswerd)
+        {
+            questionType = questiontype;
+            questionText = questiontext;    
+            questionAnswer = questionanswer;             
+            questionAnswerA = questionanswera;
+            questionAnswerB = questionanswerb;
+            questionAnswerC = questionanswerc;
+            questionAnswerD = questionanswerd;
+
+        }
+        
+        public string QuestionType { get => questionType; set => questionType = value; }
+        public string QuestionText { get => questionText; set => questionText = value; }
+        public string QuestionAnswer { get => questionAnswer; set => questionAnswer = value; }
+        public string QuestionAnswerA { get => questionAnswerA; set => questionAnswerA = value; }
+        public string QuestionAnswerB { get => questionAnswerB; set => questionAnswerB = value; }
+        public string QuestionAnswerC { get => questionAnswerC; set => questionAnswerC = value; }
+        public string QuestionAnswerD { get => questionAnswerD; set => questionAnswerD = value; }
+    }
+}
